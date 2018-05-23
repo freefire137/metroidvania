03ede2eb23a0a8affae29e2075ee545ab0541a6a
freefire137 <ianawfinch@gmail.com>
2018-05-23 02:17:44 -0400

    the beginnings of a working 2d platformer

--- Metroidvania/Assets/Scripts/Movement.cs
+++ Metroidvania/Assets/Scripts/Movement.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
+	public int jumpSpeed = 6;
+	private Vector3 jumpForce;
+	public int moveSpeed = 3;
+	private bool isGrounded;
+
 	// Use this for initialization
 	void Start () {
-		
+	}
+
+	void OnCollisionEnter() {
+		isGrounded = true;
+		Debug.Log("grounded");
+	}
+	void OnCollisionStay() {
+		//isGrounded = true;
+	}
+
+	void OnCollisionExit() {
+		isGrounded = false;
+		Debug.Log("ungrounded");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		Rigidbody myRigid = this.GetComponent<Rigidbody>();
+		myRigid.velocity = new Vector3(moveSpeed*Input.GetAxis("Horizontal"),myRigid.velocity.y,0f);
+		if (isGrounded && Input.GetKey(KeyCode.Space)) {
+			myRigid.AddForce(new Vector3(0f, jumpSpeed, 0f), ForceMode.Impulse);
+			//isGrounded = false;
+		}
+		//Camera.main
+		//Debug.Log()
 	}
 }
